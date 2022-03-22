892641d466556418ac2de134c5e3d6f99fb31947
unknown <renarasecchim@gmail.com>
2022-03-22 15:01:13 -0300

    reponsive-menu

---
+++ script.js
@@ -0,0 +1,8 @@
+const btnMobile = document.getElementById('nav-bar-btn');
+
+function toggleMenu(){
+  let menu = document.getElementById('menu-nav');
+  menu.classList.toggle('active');
+}
+
+btnMobile.addEventListener('click', toggleMenu);
