9b96cccbc1cf185de21cc32dc9cb3137c53b4aa1
Renara Secchim <renarasecchim@gmail.com>
2022-07-05 14:37:50 -0300

    feat: build stylesheets

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ function toggleMenu() {
 }
 
 btnMobile.addEventListener("click", toggleMenu);
+
+// lib scroll
+ScrollReveal({
+  origin:'top',
+  distance: '80px',
+  duration:1000,
+}).reveal(`.destaque,
+  .destaque .img-perfil,
+  #about .container-title-section,
+  #about .ilustration,
+  #Skills .container-title-section,
+  #Skills .container-languages,
+  #Skills .container-tools,
+  #Skills .container-design,
+  #projects,
+  #projects .container-outher,
+  #contact`)
