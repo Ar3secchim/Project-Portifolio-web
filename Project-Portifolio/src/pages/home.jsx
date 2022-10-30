8acd333307d9b360f0c8ee6e47fa284dcdd0cc0b
Renara <ar3secchim@DELL-ANELISE.localdomain>
2022-10-30 19:00:23 -0300

    feat: update html

--- Project-Portifolio/src/pages/home.jsx
+++ Project-Portifolio/src/pages/home.jsx
@@ -1,5 +1,12 @@
 export function Home() {
   return(
-    <nav></nav>
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Sobre</li>
+        <li>Projetos</li>
+        <li>Contato</li>
+      </ul>
+    </nav>
   )
 }
