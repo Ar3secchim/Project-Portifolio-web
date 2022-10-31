5c9264e6add20fa184fec207aab5239891b35825
Renara <ar3secchim@DELL-ANELISE.localdomain>
2022-10-31 18:50:59 -0300

    feat: toogle dark and install tailwind css

--- Project-Portifolio/src/main.jsx
+++ Project-Portifolio/src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Home } from './pages/home'
+import "/home/ar3secchim/Development/Project-Portifolio-web/Project-Portifolio/src/global.css"
 
+import { Home } from './pages/home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- Project-Portifolio/src/pages/home.jsx
+++ Project-Portifolio/src/pages/home.jsx
@@ -1,12 +1,16 @@
+import { Toggle } from "../assets/Componets/Toggle Theme"
+
 export function Home() {
   return(
     <nav>
       <ul>
-        <li>Home</li>
+        <li >Home</li>
         <li>Sobre</li>
         <li>Projetos</li>
         <li>Contato</li>
       </ul>
+
+      <Toggle />
     </nav>
   )
 }
