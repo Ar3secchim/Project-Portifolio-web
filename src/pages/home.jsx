cd884f2b24793fa19fb8259ee62e1bf6dc3eaa20
Renara Secchim <renarasecchim@gmail.com>
2022-11-01 11:53:49 -0300

    chore: initial vite

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,15 +2,6 @@ import { Toggle } from "../assets/Componets/Toggle Theme"
 
 export function Home() {
   return(
-    <nav>
-      <ul>
-        <li >Home</li>
-        <li>Sobre</li>
-        <li>Projetos</li>
-        <li>Contato</li>
-      </ul>
-
-      <Toggle />
-    </nav>
+    <div>OI</div>
   )
 }
