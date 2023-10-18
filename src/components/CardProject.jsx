3823c15dee661744d1582438784ecbc35164e2a5
ar3secchim <renarasecchim@gmail.com>
2023-10-18 11:11:23 -0300

    feat: update page project responsividade

--- src/components/CardProject.jsx
+++ src/components/CardProject.jsx
@@ -2,10 +2,10 @@ import { TitleSection } from "../components/TitleSection";
 
 export function CardProject({Img, Title, Data, Discription, children}){
   return(
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center ">
       <img className="w-3/4" src={Img}/>
 
-      <div className="flex text-start justify-items-center">
+      <div className="flex text-start justify-items-center px-16 mt-8">
         <div className="flex-0 ml-6">
           <TitleSection title={Title} ClassName="text-xl py-0"/>
 
@@ -15,7 +15,7 @@ export function CardProject({Img, Title, Data, Discription, children}){
           </div>
         </div>
 
-        <div className="flex-1 gap-2 ml-20 mt-4">
+        <div className="flex-1 gap-2 ml-20">
           <p className="text-lg pb-2">{Discription}</p>
           {children}
         </div>
