2032301c074437e278e650675e66c8a19de57167
Ar3secchim <renarasecchim@gmail.com>
2024-01-10 09:38:32 -0300

    design: responsive home and update image page contact

--- pages/components/Button/index.js
+++ pages/components/Button/index.js
@@ -1,9 +1,9 @@
 import Link from "next/link";
 
-export default function Button({ children, Route }) {
+export default function Button({ children, Route, Blank }) {
   return (
-    <button className="border-1 border-[#262626] rounded-full font-semibold text-center text-base py-1 px-4 bg-white text-[#262626]  shadow-sm hover:scale-105 transform transition-all duration-500 ease-in-out">
-      <a className="flex items-center gap-2" href={Route} target="_blank">
+    <button className="border-1 border-[#262626] rounded-full font-semibold text-center text-base py-1 px-4 bg-white text-[#262626]  shadow-sm hover:scale-105 transform transition-all duration-500 ease-in-out" >
+      <a className="flex items-center gap-2" href={Route} target={Blank ? "_blank": ""}>
         {children}
       </a>
     </button>
