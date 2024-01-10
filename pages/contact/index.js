7a2993fb385691a38f677cc20af589274c721294
Ar3secchim <renarasecchim@gmail.com>
2024-01-10 09:38:32 -0300

    design: responsive home and update image page contact

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,38 +1,41 @@
 import DefaultLayout from "../components/DefaultLayout";
 import Image from "next/image";
 import { FaAngleRight } from "react-icons/fa6";
-import ApiEcomerce from "../components/RenaraDevUI/Icons/iconsSvg/ApiEcomerce.png";
+import Avatar from "../../public/avatar.svg";
 import Button from "../components/Button";
 import { PiArrowUpRightBold } from "react-icons/pi";
 
-export default function Contact(){
+export default function Contact() {
   return (
     <DefaultLayout>
       <h1 className="inline-flex items-center gap-2 font-bold text-3xl">
         <FaAngleRight size={20} /> Contato
       </h1>
 
-      <div className="flex items-center justify-center my-24">
-        <Image
-          className="hidden md:flex lg:w-2/4"
-          src={ApiEcomerce}
-        />
-
-        <div className="flex flex-col items-center gap-4 mt-10">
-          <Button Route="https://github.com/Ar3secchim">
+      <div className="flex items-center justify-center my-24 gap-6">
+        <div className="flex-1 flex flex-col  gap-4 mt-10">
+          <Button Route="https://github.com/Ar3secchim" Blank={true}>
             GitHub
             <PiArrowUpRightBold />
           </Button>
 
-          <Button Route="https://www.linkedin.com/in/renarasecchim/">
+          <Button
+            Route="https://www.linkedin.com/in/renarasecchim/"
+            Blank={true}
+          >
             Linkedin
             <PiArrowUpRightBold />
           </Button>
 
-          <p className="font-regular text-center lg:text-xl">
+          <p className="font-regular lg:text-xl">
             renarasecchim@gmail.com
           </p>
         </div>
+
+        <Image
+          className="hidden md:flex lg:w-1/2 pb-6 px-4 bg-[#262626] bg-opacity-40 border-2 rounded-2xl border-zinc-800"
+          src={Avatar}
+        />
       </div>
     </DefaultLayout>
   );
