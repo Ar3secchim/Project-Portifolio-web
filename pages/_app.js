9e8c752ee9485070e7e874b25c838693d7e3a099
Ar3secchim <renarasecchim@gmail.com>
2024-02-02 16:48:57 -0300

    feat: add SpeedInsights the vercel

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import { Manrope } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const manrope = Manrope({
   weight: ["400", "200", "700", "800"],
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }) {
   return (
     <main className={manrope.className}>
       <Component {...pageProps} />
+      <SpeedInsights />
     </main>
   );
 }
