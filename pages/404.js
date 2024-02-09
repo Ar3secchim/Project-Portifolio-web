84f218e95e9ebe6e376ab04b6f8cbb123769381d
Ar3secchim <renarasecchim@gmail.com>
2024-02-09 16:08:53 -0300

    feat: create page error 400 and 500

---
+++ pages/404.js
@@ -0,0 +1,18 @@
+import { Alert, AlertTitle } from "@/components/ui/alert";
+import { ExclamationTriangleIcon } from "@radix-ui/react-icons";
+import DefaultLayout from "@/components/DefaultLayout";
+
+export default function Custom404() {
+  return (
+    <DefaultLayout className="h-3/5">
+      <div className="h-[600px] flex justify-center items-center">
+        <Alert>
+          <ExclamationTriangleIcon className="h-6 w-6" />
+          <AlertTitle className="m-2 font-bold text-4xl">
+            Ops!! Not found page
+          </AlertTitle>
+        </Alert>
+      </div>
+    </DefaultLayout>
+  );
+}
