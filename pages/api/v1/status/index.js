b6e01da3ece273d8ae0e39fe63d42bcc4bf52884
Renara Secchim <renarasecchim@gmail.com>
2024-02-19 17:05:29 -0300

    feat: update teste endpoint status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,18 @@
-import database from "/infra/database.js";
+import database from "infra/database.js";
 
 async function status(req, res) {
-  res.status(200).json({res: "Hellou word"});
+  const updateAt = new Date().toISOString();
+  const databaseVersion = await database.query("SHOW server_version;")
+  const databaseVersionResult= databaseVersion.rows[0].server_version;
+
+  res.status(200).json({
+    update_at: updateAt,
+    database: {
+      dependecies: {
+        version: databaseVersionResult,
+      },
+    },
+  });
 }
 
 export default status;
--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -1,4 +1,21 @@
-test("GET  to api/v1/status should return 200", async () => {
+test("should return a status code of 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toEqual(200);
+});
+
+test("should return an object with an update_at property", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+
+  const responseBody = await response.json();
+  
+  expect(responseBody.update_at).toEqual(
+    new Date(responseBody.update_at).toISOString(),
+  );
+});
+
+test("should return an object with version database", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+
+  const responseBody = await response.json();
+  expect(responseBody.database.dependecies.version).toEqual("16.0");
 });
